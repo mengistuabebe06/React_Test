19068094a69376ed9b457f8d5fff158558f02fab
mengistu abebe <ethiomengistu@gmail.com>
2024-04-10 17:09:58 +0300

    git deploy

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
    plugins: [react()],
- base:"/vite-deploy/"
-  // build: {
-  //   outDir: 'dist', // Output directory for production build
-  // },
+//  base:"/vite-deploy/"
+  build: {
+    outDir: 'dist', // Output directory for production build
+  },
   
  
 
